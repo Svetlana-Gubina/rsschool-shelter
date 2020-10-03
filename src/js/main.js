3c0a5ce56fc77a927925476b18b2cf9d2ec42608
Svetlana <gubinas83@gmail.com>
2020-10-03 17:09:52 +0300

    feat: footer mobile layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,33 @@
-console.log('Hello, world!');
+"use strict";
+(function () {
+  const BREAKPOINTS = {
+    tablet: "768px",
+  };
+
+  const toggle = document.querySelector(".main-nav__toggle");
+  const menu = document.querySelector(".main-nav");
+
+  const handleBreackpointChange = (event) => {
+    if (event.matches) {
+      menu.classList.add("visually-hidden");
+      toggle.classList.remove("visually-hidden");
+    } else {
+      menu.classList.remove("visually-hidden");
+      toggle.classList.add("visually-hidden");
+    }
+  };
+
+  const mediaQueryList = window.matchMedia(
+    "(max-width:" + BREAKPOINTS.tablet.toString() + ""
+  );
+  mediaQueryList.addListener(handleBreackpointChange);
+  handleBreackpointChange(mediaQueryList);
+
+  toggle.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    this.classList.toggle("main-nav__toggle-close");
+    menu.classList.toggle("visually-hidden");
+  });
+})();
+
+console.log("WTF?");
